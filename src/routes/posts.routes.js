74ab9883ea462af49857853d5afbea7e81cad310
code-jatin0112 <jatin.kumar.s.125@kalvium.community>
2026-02-07 11:13:09 +0530

    feat: add endpoint to get post by id

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { getAllPosts } = require('../controllers/posts.controller');
+const {
+  getAllPosts,
+  getPostById
+} = require('../controllers/posts.controller');
 
 const router = express.Router();
 
 // GET /api/v1/posts
 router.get('/posts', getAllPosts);
+
+// GET /api/v1/posts/:postId
+router.get('/posts/:postId', getPostById);
 
 module.exports = router;
